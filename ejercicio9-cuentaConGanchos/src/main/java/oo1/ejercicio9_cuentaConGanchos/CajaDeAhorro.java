44aadec2527e63220420d6fa6024bc7b0bfbed44
Julián Serio <83135918+JulianSerio@users.noreply.github.com>
2023-09-22 10:58:01 -0300

    Update CajaDeAhorro.java

--- ejercicio9-cuentaConGanchos/src/main/java/oo1/ejercicio9_cuentaConGanchos/CajaDeAhorro.java
+++ ejercicio9-cuentaConGanchos/src/main/java/oo1/ejercicio9_cuentaConGanchos/CajaDeAhorro.java
@@ -12,8 +12,21 @@ public class CajaDeAhorro extends Cuenta {
 		if ((this.getSaldo() - this.costoAdicional(monto)) - monto >= 0) return true;
 		else return false;
 	}
+	
+	public boolean extraer (double monto) {
+		return super.extraer(monto + this.costoAdicional(monto));
+	}
+	
+	public void depositar(double monto) {
+		super.depositar(monto - this.costoAdicional(monto));
+	}
 
 	protected double costoAdicional(double monto) {
-		return monto * 0.2;
+		return monto * 0.02;
 	}
+	
+	public boolean transferirACuenta (double monto, Cuenta otraCuenta) {
+		return super.transferirACuenta(monto + this.costoAdicional(monto),otraCuenta);
+	}
+	
 }
