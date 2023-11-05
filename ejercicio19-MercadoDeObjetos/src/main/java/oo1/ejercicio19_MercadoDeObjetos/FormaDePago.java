016302d1513554c55660478b93d53356dcf02aea
Julián Serio <83135918+JulianSerio@users.noreply.github.com>
2023-11-04 21:30:13 -0300

    Delete ejercicio19-MercadoDeObjetos directory

--- ejercicio19-MercadoDeObjetos/src/main/java/oo1/ejercicio19_MercadoDeObjetos/FormaDePago.java
+++
@@ -1,5 +0,0 @@
-package oo1.ejercicio19_MercadoDeObjetos;
-
-public interface FormaDePago {
-	public double precioFinal(double precio);
-}
--- ejercicio19-MercadoDeObjetos/src/main/java/oo1/ejercicio19_MercadoDeObjetos/TipoEnvio.java
+++
@@ -1,6 +0,0 @@
-package oo1.ejercicio19_MercadoDeObjetos;
-
-public interface TipoEnvio {
-	public double costo();
-
-}
